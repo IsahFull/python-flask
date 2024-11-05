ad1f2d3513c96bff1e742366c3c9b55553b61085
Isadora Sá <tavaresisadora03@gmail.com>
2024-11-04 21:06:58 -0300

    ultima aula PARAMETROS

--- microblog/app.py
+++ microblog/app.py
@@ -5,9 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def index ():
     return  render_template('index.html')
+
 @app.route('/contato')
 def contato ():
-    return render_template('contato.html')
+    return render_template('contato.html', tel = '(87)98165-6521', nome = 'Isadora Sá')
+
+@app.route('/pagina')
+def pagina ():
+    return  render_template('pagina.html')
+
+@app.route('/redes')
+def redes ():
+    return render_template('redes.html', rs = 'isa_ttavares', git = 'FullIsah')
+
+#calculadora operação parametro dinamico
+@app.route('/soma/<int:num1>/<int:num2>')
+def soma (num1, num2):
+    return f'Total: {num1 + num2}'
+
 
 if __name__ == '__main__':
     app.run()
